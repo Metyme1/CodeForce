c522b216018a017667b7504aa3705ddce3bee36c
Metyme1 <millionmety@gmail.com>
2023-11-14 16:23:45 +0300

    nov-14-2023

---
+++ gima.py
@@ -0,0 +1,25 @@
+n = int(input())
+cards = list(map(int, input().split()))
+ser =0
+dima =0
+l=0
+r=n-1
+tur =0
+
+while l<= r:
+    if tur ==0:
+        if cards[l]> cards[r]:
+            ser +=cards[l]
+            l+=1
+        else:
+            ser+=cards[r]
+            r-=1
+    else:
+        if cards[l]> cards[r]:
+            dima +=cards[l]
+            l+=1
+        else:
+            dima+=cards[r]
+            r-=1
+    tur = 1- tur
+print(ser,dima)
